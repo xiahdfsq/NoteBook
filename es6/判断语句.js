9d9de1685ef629931dff21cc4fb6cf6c84320cbf
xiahdfsq <2435068331@qq.com>
2018-05-08 23:43:02 +0800

    添加小程序的例子

---
+++ es6/判断语句.js
@@ -0,0 +1,15 @@
+Number.isFinite()是否为有限
+Number.isNaN()是否为非数值
+Number.isInteger()是否为整数
+Number.isSafeInteger()是否为计算机可以准确表达的整数范围
+Number.
+Number.
+Number.
+Number.
+Number.
+Number.
+Number.parseInt()
+Number.parseFloat()
+
+
+typeof -1 === "number"
